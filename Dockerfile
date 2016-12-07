a658a9722157c59243be4053b7898e3f021e2bbf
matt urbanski <matt.urbanski@iflowfor8hours.info>
2016-12-06 23:01:34 -0500

    begin migration post, move infra stuff

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM nginx:alpine
-ENV TERM dumb
 COPY public /usr/share/nginx/html
 RUN chown -R nginx:nginx /usr/share/nginx/html
 COPY dev/nginx.conf /etc/nginx/nginx.conf
--- dev/bake-docker-image.sh
+++ dev/bake-docker-image.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# define BASEURL in pipeline
+# Create a production and non-prod BASEURL environment variable in pipeline configuration
+
 if [ -z "${BASEURL}" ]; then 
     HUGO_BASEURL='http://localhost/'
 else 
     HUGO_BASEURL=${BASEURL}
 fi
 
-env HUGO_BASEURL=$HUGO_BASEURL hugo --cleanDestinationDir
+env HUGO_BASEURL=${HUGO_BASEURL} hugo --cleanDestinationDir
 docker build -t iflowfor8hours:blog .
 
 echo docker run --name bakedblog -p 80:80 iflowfor8hours:blog
