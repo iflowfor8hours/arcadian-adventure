bf260fe147f2a291e95bca391844e10875b455e7
matt urbanski <matt.urbanski@iflowfor8hours.info>
2017-05-01 00:46:57 -0400

    Broke SSL out to it's own container, better build structure.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM nginx:alpine
-ARG NGINX_CONF
-ENV NGINX_CONF=$NGINX_CONF
-RUN echo $NGINX_CONF > /etc/nginx/nginx.conf
+RUN \
+    apk add --update $RUNTIME_DEPS && \
+    apk add --virtual build_deps $BUILD_DEPS &&  \
+    cp /usr/bin/envsubst /usr/local/bin/envsubst && \
+    apk del build_deps
 COPY public /usr/share/nginx/html
 RUN chown -R nginx:nginx /usr/share/nginx/html
+CMD /bin/bash -c "envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"
--- dev/bake-docker-image.sh
+++ dev/bake-docker-image.sh
@@ -2,9 +2,9 @@
 # Create a production and non-prod BASEURL environment variable in pipeline configuration
 set -e
 
-if [ -z "${BASEURL}" ]; then 
+if [ -z "${BASEURL}" ]; then
     HUGO_BASEURL='https://localhost/'
-else 
+else
     HUGO_BASEURL=${BASEURL}
 fi
 
@@ -12,4 +12,4 @@ env HUGO_BASEURL=${HUGO_BASEURL} hugo --cleanDestinationDir
 docker build -t iflowfor8hours:blog .
 
 echo HUGO_BASEURL=${HUGO_BASEURL}
-echo docker run --rm --name bakedblog -p 80:80 -p 443:443 iflowfor8hours:blog
+echo docker run --rm --name bakedblog -p 80:80 iflowfor8hours:blog
